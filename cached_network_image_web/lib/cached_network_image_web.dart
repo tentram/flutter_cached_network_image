176ea5e5a4ed704ba4036a343df4fa89a0e0fd9c
Bagus <25834188+contactjavas@users.noreply.github.com>
2023-09-04 23:43:45 +0700

    Revert "Update cached_network_image_web.dart"
    This reverts commit e66ab215df3e9bcfb339b390a6952e56ad3cb116.

--- cached_network_image_web/lib/cached_network_image_web.dart
+++ cached_network_image_web/lib/cached_network_image_web.dart
@@ -51,7 +51,7 @@ class ImageLoader implements platform.ImageLoader {
     String url,
     String? cacheKey,
     StreamController<ImageChunkEvent> chunkEvents,
-    ImageDecoderCallback decode,
+    DecoderBufferCallback decode,
     BaseCacheManager cacheManager,
     int? maxHeight,
     int? maxWidth,
