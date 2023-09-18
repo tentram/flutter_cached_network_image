47fa6fe12e22042dd2d5da44fce757c2d0a1a1ba
Bagus <25834188+contactjavas@users.noreply.github.com>
2023-09-18 14:06:59 +0700

    try to implement PR by Carapacik from original repo: https://github.com/Baseflow/flutter_cached_network_image/pull/861

--- cached_network_image_platform_interface/lib/cached_network_image_platform_interface.dart
+++ cached_network_image_platform_interface/lib/cached_network_image_platform_interface.dart
@@ -1,3 +1,4 @@
+/// Platform interface for CachedNetworkImage
 library cached_network_image_platform_interface;
 
 import 'dart:async';
@@ -21,25 +22,7 @@ enum ImageRenderMethodForWeb {
 class ImageLoader {
   /// loads the images async and gives the resulted codecs on a Stream. The
   /// Stream gives the option to show multiple images after each other.
-  @Deprecated('use loadBufferAsync instead')
-  Stream<ui.Codec> loadAsync(
-    String url,
-    String? cacheKey,
-    StreamController<ImageChunkEvent> chunkEvents,
-    DecoderCallback decode,
-    BaseCacheManager cacheManager,
-    int? maxHeight,
-    int? maxWidth,
-    Map<String, String>? headers,
-    Function()? errorListener,
-    ImageRenderMethodForWeb imageRenderMethodForWeb,
-    Function() evictImage,
-  ) {
-    throw UnimplementedError();
-  }
-
-  /// loads the images async and gives the resulted codecs on a Stream. The
-  /// Stream gives the option to show multiple images after each other.
+  @Deprecated('Use loadImageAsync instead')
   Stream<ui.Codec> loadBufferAsync(
     String url,
     String? cacheKey,
@@ -49,9 +32,27 @@ class ImageLoader {
     int? maxHeight,
     int? maxWidth,
     Map<String, String>? headers,
-    Function()? errorListener,
+    VoidCallback? errorListener,
     ImageRenderMethodForWeb imageRenderMethodForWeb,
-    Function() evictImage,
+    VoidCallback evictImage,
+  ) {
+    throw UnimplementedError();
+  }
+
+  /// loads the images async and gives the resulted codecs on a Stream. The
+  /// Stream gives the option to show multiple images after each other.
+  Stream<ui.Codec> loadImageAsync(
+    String url,
+    String? cacheKey,
+    StreamController<ImageChunkEvent> chunkEvents,
+    ImageDecoderCallback decode,
+    BaseCacheManager cacheManager,
+    int? maxHeight,
+    int? maxWidth,
+    Map<String, String>? headers,
+    ImageErrorListener? errorListener,
+    ImageRenderMethodForWeb imageRenderMethodForWeb,
+    VoidCallback evictImage,
   ) {
     throw UnimplementedError();
   }
